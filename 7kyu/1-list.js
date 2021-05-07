18a6ec9ca341be43d715baaa660129ee87b270f3
MariusBudreika <mariusbudreika09@gmail.com>
2021-05-07 12:38:55 +0300

    a

---
+++ 7kyu/1-list.js
@@ -0,0 +1,8 @@
+function myLanguages(results) {
+         
+}
+
+
+console.log(myLanguages({"Java" : 10, "Ruby" : 80, "Python" : 65}))
+console.log(myLanguages({"Hindi" : 60, "Greek" : 71, "Dutch" : 93}))
+console.log(myLanguages({"C++" : 50, "ASM" : 10, "Haskell" : 20}), [])
